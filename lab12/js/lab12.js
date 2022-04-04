e70f6bbb6711192d849b4005e70435b34eb9b3f2
AndresCardenas3 <78111915+AndresCardenas3@users.noreply.github.com>
2022-04-04 06:16:26 -0700

    lab 15

--- lab12/js/lab12.js
+++ lab12/js/lab12.js
@@ -29,3 +29,57 @@ $("#results-button").click(function(){
 	// toggle orange class for text
   $("#body-text").toggleClass("orange");
 })
+
+//gap
+
+// find challenge button and add click handler
+$("#challenge-button").click(function(){
+	// remove blue class and orange
+  $("#body-text2").removeClass("blue").removeClass("orange");
+	// toggle pink class for text
+  $("#body-text2").toggleClass("pink");
+})
+
+
+// find problems button and add click handler
+$("#problems-button").click(function(){
+	// remove pink class and orange class
+  $("#body-text2").removeClass("pink").removeClass("orange");
+	// toggle blue class for text
+  $("#body-text2").toggleClass("blue");
+})
+
+// find results button and add click handler
+$("#results-button").click(function(){
+	// remove pink class and blue class
+  $("#body-text2").removeClass("pink").removeClass("blue");
+	// toggle orange class for text
+  $("#body-text2").toggleClass("orange");
+})
+
+//gap
+
+// find challenge button and add click handler
+$("#challenge-button").click(function(){
+	// remove blue class and orange
+  $("#body-text3").removeClass("blue").removeClass("orange");
+	// toggle pink class for text
+  $("#body-text3").toggleClass("pink");
+})
+
+
+// find problems button and add click handler
+$("#problems-button").click(function(){
+	// remove pink class and orange class
+  $("#body-text3").removeClass("pink").removeClass("orange");
+	// toggle blue class for text
+  $("#body-text3").toggleClass("blue");
+})
+
+// find results button and add click handler
+$("#results-button").click(function(){
+	// remove pink class and blue class
+  $("#body-text3").removeClass("pink").removeClass("blue");
+	// toggle orange class for text
+  $("#body-text3").toggleClass("orange");
+})
