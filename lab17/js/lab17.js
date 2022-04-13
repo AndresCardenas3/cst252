6c2e7722649889c40270ea3a1c0a153b5d836c6b
AndresCardenas3 <78111915+AndresCardenas3@users.noreply.github.com>
2022-04-13 02:51:31 -0700

    lab 17

---
+++ lab17/js/lab17.js
@@ -0,0 +1,30 @@
+/*
+ * Author: Andres Cardenas <ancardenas@csumb.edu>
+ * Created: 12 April
+ * License: Public Domain
+ */
+
+function Vehicle(make, model, year, color, extras, name) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.color = color;
+  this.extras = extras;
+  this.name = name;
+  this.info = function() {
+    var str = "A " + color + " " + year + " " + make + " " + model + " that is a " + extras + " called " + name + ".";
+    return str;
+  }
+}
+
+var vehicles = [];
+
+vehicles.push(new Vehicle("Volkswagen", "Jetta", 2013, "white", "sedan", "my car"));
+vehicles.push(new Vehicle("Fiat", "500x", 2015, "black", "crossover", "my sister's car"));
+vehicles.push(new Vehicle("Honda", "Accord", 2018, "black", "sedan", "my dad's car"));
+vehicles.push(new Vehicle("Tesla", "Model S", 2020, "white", "sedan", "my mom's car"));
+
+outputEl = document.getElementById("output");
+for (var i = 0; i<vehicles.length; i++) {
+  $("#output").append("<li>" + vehicles[i].info())
+}
